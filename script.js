1a90f6cfe91d026560d6f6f4e2513834e627aec5
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-21 12:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-//your JS code here. If required.
-const inputs = document.querySelectorAll('.code');
+const inputs = document.querySelectorAll(".code");
+
+// Auto-focus first input on load
+window.addEventListener("load", () => inputs[0].focus());
 
 inputs.forEach((input, index) => {
-  input.addEventListener('input', (e) => {
+  input.addEventListener("input", (e) => {
     const value = e.target.value;
-    if (value) {
-      if (index < inputs.length - 1) {
-        inputs[index + 1].focus();
-      }
+
+    // Allow only digits
+    if (!/^\d$/.test(value)) {
+      e.target.value = '';
+      return;
+    }
+
+    if (index < inputs.length - 1) {
+      inputs[index + 1].focus();
     }
   });
 
-  input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-      if (!input.value && index > 0) {
-        inputs[index - 1].value = '';
-        inputs[index - 1].focus();
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "") {
+        if (index > 0) {
+          inputs[index - 1].value = '';
+          inputs[index - 1].focus();
+        }
+      } else {
+        input.value = '';
       }
     }
   });
